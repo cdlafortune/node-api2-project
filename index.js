927df6853c0de5c3d4862abb965f076551166f95
cdsuddreth <cdsuddreth@gmail.com>
2020-08-07 21:59:26 -0400

    MVP

--- index.js
+++ index.js
@@ -5,8 +5,108 @@ const server = express();
 
 server.use(express.json());
 
+//GET
+//get all posts
+server.get("/posts", (req, res) => {
+  const posts = db.find();
 
+  return posts;
+});
 
-server.listen(5000, () => {
-    console.log("Server is running on http://localhost:5000");
-  });
+//get post by ID
+server.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = db.findById(id);
+
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).json({ errorMessage: "Post not found." });
+  }
+});
+
+//POST
+//create new post
+server.post("/posts", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide a title and content for the post.",
+      });
+
+    return;
+  }
+
+  try {
+    const newPost = db.insert(req.body);
+    res.status(201).json(newPost);
+  } catch {
+    res
+      .status(500)
+      .json({
+        errorMessage:
+          "There was an error while saving this post to the database.",
+      });
+  }
+});
+
+//create comment on posts
+server.post("/posts/:id/comments", (req, res) => {
+  if (!req.body.text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide a text for the comment." });
+
+    return;
+  }
+
+  try {
+    const newComment = db.insertComment(req.body);
+    res.status(201).json(newComment);
+  } catch {
+    res
+      .status(500)
+      .json({
+        errorMessage:
+          "There was an error while saving this comment to the database.",
+      });
+  }
+});
+
+//PUT
+//update a post
+server.put("/posts/:id", (req, res) => {
+  const post = db.findById(req.params.id);
+
+  if (!post) {
+    res.status(404).json({ errorMessage: "Post not found." });
+  } else if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide a title and content for the post.",
+      });
+
+    return;
+  }
+
+  try {
+    const updatedPost = db.update(req.params.id, req.body);
+    res.status(201).json(updatedPost);
+  } catch {
+    res
+      .status(500)
+      .json({
+        errorMessage:
+          "There was an error while saving this post to the database.",
+      });
+  }
+});
+
+//DELETE
+//delete a post
+
+server.listen(6000, () => {
+  console.log("Server is running on http://localhost:5000");
+});
